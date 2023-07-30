fbe7a7081e3ef986ab65860b6807997453446d8e
Ania Płecka <aniaplecka@gmail.com>
2023-07-30 15:16:35 +0200

    Cars increase speed

--- main.py
+++ main.py
@@ -22,16 +22,21 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
     if player.ycor() > 280:
         scoreboard.track_level()
         player.reset_position()
+        car_manager.level_up()
 
-    # if player.distance(car_manager) < 20:
-    #     game_is_on = False
-    #     scoreboard.game_over()
+
 
 
 screen.exitonclick()
